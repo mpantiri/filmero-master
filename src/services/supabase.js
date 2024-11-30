9cffaa07dea1d52aa1bd7c1ac2602083fc975c6e
marina.pantiri <mpantiri@gmail.com>
2024-11-30 23:00:02 +0200

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vaqzgpnwmmlzdzfokmgx.supabase.co";
+const supabaseUrl = "https://nyrbchbhngqiorsgmoyv.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhcXpncG53bW1semR6Zm9rbWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyODk1ODEsImV4cCI6MjA0Nzg2NTU4MX0.PRV8rNE4O5wNrAHXYaSgi_ffo3YcG5R3gkC3koCIBhM";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cmJjaGJobmdxaW9yc2dtb3l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5OTYxNTksImV4cCI6MjA0ODU3MjE1OX0.BQ9VvlVBBL38jNAT6uZrAz8iph-QIbZdSP8erDL5H30";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabaseUrl };
